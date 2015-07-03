58fd788a5f9051e68f324abb0b936c73655d57b2
Daniel <danieljr.junior7@gmail.com>
2015-07-02 21:40:52 -0300

    site base

---
+++ index.php
@@ -0,0 +1,125 @@
+
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+    <title>SITE EDSON E DANIEL</title>
+    <link rel="stylesheet" href="style.css" type="text/css" charset="utf-8" />
+
+    <style>		#my-header-new a,#my-header-new a:hover {color:#c2cae0;}#my-header-new a:hover {text-decoration:none}</style>
+
+</head>
+
+<body>
+
+    <div id="outer-wrapper">
+        <div id="structure">
+            <!--TERMS OF USE. DON'T EDIT OR DELETE THESE LINES. IF YOU EDIT OR DELETE THESE LINES AN ALERT MESSAGE MAY APPEAR WHEN TEMPLATE WILL BE ONLINE-->
+            <div id="tcopy" style="height: 75px; position: absolute; bottom: 0px; left:0px; border: none; width: 100%;"><br/><br/><br/>
+                <div id="my-header-new" style="width:820px;margin:0 auto;text-align:right;position:relative;bottom:0px;margin-top:63px;color:#B9B9B9;font-size:10px;font-family:Verdana"><strong>vídoes de maquiagem</strong> no <a href="http://www.maquiagempassoapasso.org/"><strong>maquiagempassoapasso.org</strong></a></div>																			 
+            </div>	
+            <!--TERMS OF USE-->
+
+
+            <div id="wrapper" style="
+                 padding-left: 50px;
+                 padding-right: 50px;
+                 ">
+               <?php include 'menu.php'; ?>
+                <div id="topcon">
+                    <div id="topcon-inner">
+                        <h2>Bem vindo ao Site da musica</h2>
+                        <p>Aqui voçê pode ouvir e baixar a sua musica e ficar sabendo tudo sobre o seu artista preferido. Então divirta-se</p>
+
+                    </div>
+                </div>
+                <div id="content">
+                    <div id="body">
+                        <div class="box" id="news">
+                            <div class="box-t"><div class="box-r"><div class="box-b"><div class="box-l">
+                                            <div class="box-tr"><div class="box-br"><div class="box-bl"><div class="box-tl">
+
+                                                            <h2>NOTICIAS E EVENTOS</h2>
+
+                                                            <h3></h3>
+                                                            <p>Show de Luan Santana - São João de ireçê Irecê-BA <a href=""></a></p>
+                                                            <p class="more"><a href="http://www.luansantana.com.br ">Saiba mais</a></p>
+
+                                                            <div class="hr-yellow"> </div>
+
+                                                            <h3></h3>
+                                                            <p> Câmara de SP aprova projeto que proíbe aplicativo Uber<a href=""></a></p>
+                                                            <p class="more"><a href="http://g1.globo.com/sao-paulo/noticia/2015/06/camara-de-sp-aprova-projeto-que-proibe-aplicativo-uber.html"> Saiba mais...</a></p>
+                                                           
+
+                                                        </div></div></div></div>
+                                        </div></div></div></div>
+                        </div>
+                        <div class="box" id="hits">
+                            <div class="box-t"><div class="box-r"><div class="box-b"><div class="box-l">
+                                            <div class="box-tr"><div class="box-br"><div class="box-bl"><div class="box-tl">
+
+                                                            <h2>SUCESSOS</h2>
+
+                                                            <h3>Só as Antigas</h3>
+                                                            <img src="images/pic_1.jpg" width="63" height="91" alt="Pic 1" class="right" />
+                                                            <ul>
+                                                                <li><a href=" ">Lorem ipsum dolor s........</a></li>
+                                                                <li><a href=" ">Vivamus lobortis p........ </a></li>
+                                                                <li><a href=" ">Sit amet, consectetu.......</a></li>
+                                                                <li><a href=" ">Lorem ipsum dolor s........</a></li>
+                                                                <li><a href=" ">Vivamus lobortis p........ </a></li>
+                                                            </ul>
+
+                                                            <div class="hr-yellow"> </div>
+
+                                                            <h3>Século XXl</h3>
+                                                            <img src="images/pic_2.jpg" width="63" height="81" alt="Pic 2" class="right" />
+                                                            <ul>
+                                                                <li><a href=" ">Vivamus lobortis p........ </a></li>
+                                                                <li><a href=" ">Lorem ipsum dolor s........</a></li>
+                                                                <li><a href=" ">Vivamus lobortis p........ </a></li>
+                                                                <li><a href=" ">Sit amet, consectetu.......</a></li>
+                                                            </ul>
+
+                                                        </div></div></div></div>
+                                        </div></div></div></div>
+                        </div>
+                        <div class="box" id="new">
+                            <div class="box-t"><div class="box-r"><div class="box-b"><div class="box-l">
+                                            <div class="box-tr"><div class="box-br"><div class="box-bl"><div class="box-tl">
+
+                                                            <h2>LANÇAMENTOS</h2>
+
+                                                            <h3>eestas sed</h3>
+                                                            <img src="images/pic_3.jpg" width="66" height="52" alt="Pic 3" class="right" />
+                                                            <p>porta vel, nulla. Maecenas condi- mentum sollicitu</p>
+
+                                                            <h3>Maecenas</h3>
+                                                            <img src="images/pic_4.jpg" width="66" height="52" alt="Pic 4" class="right" />
+                                                            <p>ndimentum sollicitudin quam. Sed convallis</p>
+
+                                                            <h3>sollicitudin</h3>
+                                                            <img src="images/pic_5.jpg" width="66" height="52" alt="Pic 5" class="right" />
+                                                            <p>quam. Sed nvallis lacus .vallislacus arcu.vallis</p>
+
+                                                        </div></div></div></div>
+                                        </div></div></div></div>
+                        </div>
+                        <div class="clear"> </div>
+                    </div>
+                    <div id="footer">
+                        <ul>
+                            <li><a href=" ">fotos</a> | </li>
+                            <li><a href=" ">musicas gratis</a> | </li>
+                            <li><a href=" ">estilos</a> | </li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
---
+++ sobre.php
@@ -0,0 +1,62 @@
+
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+    <title>SITE EDSON E DANIEL</title>
+    <link rel="stylesheet" href="style.css" type="text/css" charset="utf-8" />
+
+    <style>		#my-header-new a,#my-header-new a:hover {color:#c2cae0;}#my-header-new a:hover {text-decoration:none}</style>
+
+</head>
+
+<body>
+
+    <div id="outer-wrapper">
+        <div id="structure">
+            <!--TERMS OF USE. DON'T EDIT OR DELETE THESE LINES. IF YOU EDIT OR DELETE THESE LINES AN ALERT MESSAGE MAY APPEAR WHEN TEMPLATE WILL BE ONLINE-->
+            <div id="tcopy" style="height: 75px; position: absolute; bottom: 0px; left:0px; border: none; width: 100%;"><br/><br/><br/>
+                <div id="my-header-new" style="width:820px;margin:0 auto;text-align:right;position:relative;bottom:0px;margin-top:63px;color:#B9B9B9;font-size:10px;font-family:Verdana"><strong>vídoes de maquiagem</strong> no <a href="http://www.maquiagempassoapasso.org/"><strong>maquiagempassoapasso.org</strong></a></div>																			 
+            </div>	
+            <!--TERMS OF USE-->
+
+
+            <div id="wrapper" style="
+                 padding-left: 50px;
+                 padding-right: 50px;
+                 ">
+                <?php include 'menu.php'; ?>
+                <div id="topcon">
+                    <div id="topcon-inner">
+                        <h2> SOBRE </h2>
+                        <p>O site Musicaqui oferece o melhor da musica para voçê</p>
+                        <p> Aqui voçê pode ouvir sua musica favorita</p>
+
+                    </div>
+                </div>
+                <div id="content">
+                    <div id="body">
+                        novo conteudo da pagina sobre
+                        <p>asdadasa</p>
+                        <p>asdadasa</p>
+                        <p>asdadasa</p>
+                        <p>asdadasa</p>
+                        <p>asdadasa</p>
+
+                        <div class="clear"> </div>
+                    </div>
+                    <div id="footer">
+                        <ul>
+                            <li><a href=" ">fotos</a> | </li>
+                            <li><a href=" ">musicas gratis</a> | </li>
+                            <li><a href=" ">estilos</a> | </li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
